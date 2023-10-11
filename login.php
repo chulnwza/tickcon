06de16876bcef6d1ddec23834890a095b0dae6f0
TIMEMIXCELL <nattapal2547@gmail.com>
2023-10-11 17:35:27 +0700

    Update login.php

--- login.php
+++ login.php
@@ -6,14 +6,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Login</title>
-
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+        .container-box {
+            text-align: center;
+            border: 2px solid grey;
+            border-radius: 25px;
+            padding: 3%;
+            width: 40vh;
+        }
+        .btn-primary{
+            background-color: grey;
+        }
+    </style>
 </head>
 
 <body class="p-5">
-    <div class="container my-5">
+    <div class="position-absolute top-50 start-50 translate-middle container-box">
         <form action="signin_db.php" method="post">
         <?php if (isset($_SESSION['error'])) {?>
                 <div class="alert alert-danger" role="alert">
@@ -31,24 +45,24 @@
             <?php }?>
             <h1>เข้าสู่ระบบ</h1>
             <div class="form-outline mb-4">
-                <i class='trailing'></i>
-                <input type="email" id="form" class="form-control" name="email"/>
-                <label class="form-label">อีเมล</label>
+                <i class='trailing'>@</i>
+                <input type="email" id="form" class="form-control" />
+                <label class="form-label">ที่อยู่อีเมล</label>
             </div>
             <div class="form-outline mb-4">
                 <i class='bx bxs-lock-alt trailing'></i>
-                <input type="password" id="form" class="form-control" name="password"/>
+                <input type="password" id="form" class="form-control" />
                 <label class="form-label">รหัสผ่าน</label>
             </div>
 
-            <button type="submit" class="btn btn-primary btn-block mb-4" name="signin">เข้าสู่ระบบ</button>
+            <button type="submit" class="btn btn-primary btn-block mb-4">เข้าสู่ระบบ</button>
             <div class="text-center">
-                <p>ยังไม่มีบัญชีใช่มั้ย? <a href="register.php"> สมัครสมาชิก</a></p>
+                <p>ยังไม่มีบัญชีผู้ใช้? <a href="register.php"> ลงทะเบียน</a></p>
             </div>
         </form>
     </div>
 
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
 </body>
 
 </html>
