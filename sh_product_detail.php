e72bfb8be311b693c0f848ebafe32b623355f2f9
Apiveb <109953646+Apiveb@users.noreply.github.com>
2023-10-22 19:14:28 +0700

    Update sh_product_detail.php

--- sh_product_detail.php
+++ sh_product_detail.php
@@ -8,6 +8,7 @@ include 'condb.php'
     <title>Tickcon</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         hr {
             color: brown;
@@ -104,15 +105,21 @@ include 'condb.php'
                                             <div class="col-3 my-2">
                                                 <p class="fw-bold fs-6 my-0">Price</p>
                                             </div>
+                                    </li>
                 <?php
-    $sql1 = 'SELECT * FROM ticket_detail
-    WHERE concert_id= "'.$ids.'";';
-    $result = $db->query($sql1);
-    while($row1 = $result->fetchArray(SQLITE3_ASSOC) ) {
+    $sql1 = 'SELECT *, count(detail_id) `amount_each`
+    FROM (SELECT detail_id, name, description, price, payment_id, amount
+    FROM ticket_detail
+    JOIN ticket
+    USING (detail_id)
+    WHERE concert_id= '.$ids.')
+	GROUP by detail_id, payment_id;';
+    $result1 = $db->query($sql1);
+    while($row1 = $result1->fetchArray(SQLITE3_ASSOC) ) {
+        if ((is_null($row1['payment_id']) == TRUE) || ((is_null($row1['payment_id']) == FALSE) && ($row1['amount'] == $row1['amount_each']))) {
     ?>
                                         <li class="list-group-item">
                                         <div class="row border border-top-0">
-                                            <!--แบ่งเป็น 8/12 ไว้แสดงข้อมูล-->
                                             <div class="col-6 align-items-center my-1">
                                             <h6 class="my-0 fw-bold" id="type"><?=$row1['name']?></h6>
                                                 <?php
@@ -124,21 +131,111 @@ include 'condb.php'
                                                 <small class="text-secondary my-0">'<?=$ticket_det1?></small>
                                             </div>
                                             <div class="col-3 d-flex align-items-center text-center">
-                                                <!--ใช้เป็น selected เพราะจะได้เห็นค่าที่เลือก-->
                                                 <p class="my-0 fs-6"><?=$row1['price']?></p>
                                             </div>
                                             <div class="col-3 d-flex align-items-center text-center my-1">
+                                                
                                                 <button type="button" class="btn btn-danger" data-bs-toggle="modal"
                                                 data-bs-target="#payment"
                                                 data-ticket-name="<?=$row1['name']?>"
                                                 data-ticket-description="<?=$row1['description']?>"
-                                                data-ticket-price="<?=$row1['price']?>">Buy Now</button>
+                                                data-ticket-price="<?=$row1['price']?>"
+                                                data-ticket-detail-id="<?=$row1['detail_id']?>"
+                                                <?php
+                                                if ((is_null($row1['payment_id']) == FALSE) && ($row1['amount'] == $row1['amount_each'])) {
+                                                    echo 'disabled> Sold Out </button>';
+                                                } else {
+                                                    echo '>Buy Now</button>';
+                                                }
+                                                ?>
                                             </div>
                                         </div>
                                     </li>
     <?php
-    }
+    }}
     ?>
+    <div class="modal fade" id="payment" tabindex="-1" role="dialog" aria-labelledby="Title"
+                                aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <!--ส่วนหัว-->
+                                        <div class="modal-header">
+                                            <p class="modal-title fw-bold fs-6" id="Title">Buy ticket</p>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close">
+                                            </button>
+                                        </div>
+                                        <!--ส่วนฟอร์ม-->
+                                        <div class="modal-body">
+                                            <div class="card mb-4">
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="col-4">
+                                                            <img class="rounded rounded-4 border border-3 border-dark w-100"
+                                                                src="toei_backend/<?=$row['concert_img_path']?>" id="buying-poster" alt="poster">
+                                                        </div>
+                                                        <div class="col-8 d-flex align-items-center">
+                                                            <ul class="list-unstyled">
+                                                                <li class="mb-2"><p class="my-0 fw-bold">ประเภทที่นั่ง:&nbsp;</p><span id="modal-ticket-name"></span></li>
+                                                                <li class="mb-2"><p class="my-0 fw-bold">รายละเอียด:&nbsp;</p><span id="modal-ticket-description"></span></li>
+                                                                <li class="mb-2"><p class="my-0 fw-bold">ราคา:&nbsp;</p><span id="modal-ticket-price"></span></li>
+                                                                <li class="mb-2"><p class="my-0 fw-bold">วันที่และเวลา:&nbsp;</p><?=date('l d F Y', strtotime($row['show_date']));?>, <?=$row['show_time']?></li>
+                                                                <li class="mb-2"><p class="my-0 fw-bold">สถานที่จัด:&nbsp;</p><?=$row['address']?></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <form action="" method="POST">
+                                                <div class="form-floating mb-2">
+                                                    <input type="text" class="form-control" id="credit_number"
+                                                        oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+                                                        placeholder="เลขบัตรเครดิต" name="credit_number">
+                                                    <label for="credit_number">เลขบัตรเครดิต</label>
+                                                </div>
+                                                <div class="form-floating mb-2">
+                                                    <input type="text" class="form-control" id="credit_name"
+                                                        placeholder="ชื่อ-นามสกุลผู้ถือบัตร" name="credit_name">
+                                                    <label for="credit_name">ชื่อ-นามสกุลผู้ถือบัตร</label>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-4 col-sm-6">
+                                                        <div class="form-floating mb-2">
+                                                            <input type="text" id="form" name="credit_month"
+                                                                class="form-control"
+                                                                oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+                                                                maxlength="2" placeholder="Month" name="credit_month" />
+                                                            <label for="credit_month">MM</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-4 col-sm-6">
+                                                        <div class="form-floating mb-2">
+                                                            <input type="text" id="form" name="credit_year"
+                                                                class="form-control"
+                                                                oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+                                                                maxlength="4" placeholder="Year" name="credit_year" />
+                                                            <label for="credit_year">YY</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-4 col-sm-12 text-center">
+                                                        <div class="form-floating mb-2">
+                                                            <input type="text" id="form" name="credit_cvv"
+                                                                class="form-control"
+                                                                oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+                                                                maxlength="3" placeholder="CVV" name="credit_cvv" />
+                                                            <label for="credit_cvv">CVV</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!--ส่วนท้าย-->
+                                                <div class="modal-footer">
+                                                <button type="submit" class="btn btn-danger" name="confirm">Confirm</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                                         </div>
                                     </li>
                                 </ul>
@@ -154,4 +251,13 @@ include 'condb.php'
     $db->close();
     ?>
 </body>
+<script>
+    $('#payment').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var modal = $(this);
+        modal.find('#modal-ticket-name').text(button.data('ticket-name'));
+        modal.find('#modal-ticket-description').text(button.data('ticket-description'));
+        modal.find('#modal-ticket-price').text(button.data('ticket-price'));
+    });
+</script>
 </html>
